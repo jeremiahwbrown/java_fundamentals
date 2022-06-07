bcd6a0d27ec76a380200794d01a2758a17bb8543
Jeremiah Brown <jeremiahbrown@Jeremiahs-MacBook-Pro.local>
2022-06-07 15:55:10 -0700

    Exercise 5 complete  Please enter the commit message for your changes. Lines starting

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,8 @@
 package labs_examples.fundamentals.labs;
 
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -11,17 +13,44 @@ package labs_examples.fundamentals.labs;
 public class Exercise_05 {
 
     public static void main(String[] args) {
+        int s = 5;
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int c = str.indexOf('!');
+        int a = str.length();
+
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
 
+        boolean strEqual = true;
+
+        if(str.length() == str2.length()) System.out.println(" \"hello\" is equal to \"hello!\" ");
+        else {
+            System.out.println(" \"hello\" is not equal to \"hello!\"");
+        }
+
+
         // please concatenate str & str2 and set the result to a new String variable below
+
+        String str3  = "hello!" + "hello";
+
+        int b = str3.length();
+
+        System.out.println("the value of str = 6 and the value of str2 = 5");
+
+        
+
+
+
+
+
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+
 
     }
 
