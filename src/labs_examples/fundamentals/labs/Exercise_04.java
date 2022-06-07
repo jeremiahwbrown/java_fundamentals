71137dc540ab7b509765f8b867fa6f6bf19458d6
Jeremiah Brown <jeremiahbrown@Jeremiahs-MacBook-Pro.local>
2022-06-07 13:50:37 -0700

    Exercises 3 and 4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,8 +12,24 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        boolean bVal = true;
+        byte byteVal = 7;
+        char charVal = 'j';
+        short shortVal = 20002;
+        int i = 3000000;
+        long longVal = 2083400230;
+        float pieVal = 3.14f;
+        double eVal = 2.71;
+
+        System.out.println("the numbers are: ");
+        System.out.println(byteVal);
+        System.out.println(i);
+        System.out.println(shortVal);
+        System.out.println(longVal);
+        System.out.println(pieVal);
+        System.out.println(eVal);
+        System.out.println(byteVal);
+        System.out.println(bVal);
 
         // write your code below
 
