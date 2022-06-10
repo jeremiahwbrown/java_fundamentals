4d9947850a4d4ecc0be35ffb47d16c59f4636eed
Jeremiah Brown <jeremiahbrown@Jeremiahs-MacBook-Pro.local>
2022-06-10 09:08:19 -0700

    Exercises 6 and 7

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 6: Basic while loop
  *
@@ -8,4 +10,18 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("Pick a number between 1 - 50:");
+        a = scanner.nextInt();
+        System.out.println("Pick a number beteen 1 - 50:");
+        b = scanner.nextInt();
+
+        int sum = a + b;
+            System.out.println(sum);
+
+
+    }
 }
--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
  *
@@ -14,4 +16,27 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_07 {
-}
+    public static void main(String[] args) {
+        System.out.println("enter a word containing at least one vowel:");
+        Scanner scanner = new Scanner(System.in);
+
+        String str;
+        str = scanner.nextLine();
+        System.out.println(str);
+        if((str.indexOf("a") > 0) && (str.indexOf("a") < str.indexOf("e")) && (str.indexOf("a") < str.indexOf("i")) && (str.indexOf("a") < str.indexOf("o")) && (str.indexOf("a") < str.indexOf("u")))
+            System.out.println("a");
+        else if ( (str.indexOf("e") > 0) && (str.indexOf("e") < str.indexOf("a")) && (str.indexOf("e") < str.indexOf("i")) && (str.indexOf("e") < str.indexOf("o")) && (str.indexOf("e") < str.indexOf("u")))
+            System.out.println("e");
+        else if ( (str.indexOf("o") > 0) && (str.indexOf("o") < str.indexOf("a")) && (str.indexOf("o") < str.indexOf("i")) && (str.indexOf("o") < str.indexOf("e")) && (str.indexOf("o") < str.indexOf("u")))
+            System.out.println("o");
+        else if ((str.indexOf("u") > 0) && (str.indexOf("u") < str.indexOf("e")) && (str.indexOf("u") < str.indexOf("i")) && (str.indexOf("u") < str.indexOf("o")) && (str.indexOf("u") < str.indexOf("a")))
+            System.out.println("u");
+        else if (((str.indexOf("i") > 0) && (str.indexOf("i") < str.indexOf("e")) && (str.indexOf("i") < str.indexOf("o")) && (str.indexOf("i") < str.indexOf("u")) && (str.indexOf("i") < str.indexOf("a"))))
+            System.out.println("i");
+        }
+
+    }
+
+
+
+
